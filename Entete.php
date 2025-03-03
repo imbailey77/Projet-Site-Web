5ac7eebbd9ccfa3fd450ca1bd58df409db98faf7
Moumene lyna <117190428+imbailey77@users.noreply.github.com>
2025-03-03 00:53:13 -0500

    Suppression  du chargement
    Fonction incorrectement.

--- Entete.php
+++ Entete.php
@@ -15,28 +15,3 @@
             </ul>
         </nav>
 </header>
-<div id="conteneur-chargement">
-    <div id="chargement-wifi">
-        <svg class="cercle-externe" viewBox="0 0 86 86">
-            <circle class="arriere" cx="43" cy="43" r="40"></circle>
-            <circle class="avant" cx="43" cy="43" r="40"></circle>
-        </svg>
-        <svg class="cercle-milieu" viewBox="0 0 60 60">
-            <circle class="arriere" cx="30" cy="30" r="27"></circle>
-            <circle class="avant" cx="30" cy="30" r="27"></circle>
-        </svg>
-        <svg class="cercle-interne" viewBox="0 0 34 34">
-            <circle class="arriere" cx="17" cy="17" r="15"></circle>
-            <circle class="avant" cx="17" cy="17" r="15"></circle>
-        </svg>
-        <div class="texte" data-text="Chargement...">Chargement...</div>
-    </div>
-</div>
-
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        setTimeout(() => {
-            document.getElementById("conteneur-chargement").style.display = "none";
-        }, 4000);
-    });
-</script>
